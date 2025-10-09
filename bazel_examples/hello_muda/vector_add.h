de05ca0bf980c2ca0db5d383f663b8b8815da3b5
zhiguo-rbs <zhiguo.tang@roboscience.ai>
2025-10-09 15:11:31 +0800

    first work

--- bazel_examples/hello_muda/vector_add.h
+++ bazel_examples/hello_muda/vector_add.h
@@ -2,4 +2,4 @@
 #include <muda/muda.h>
 #include <muda/container.h>
 
-void vector_add(HostVector<float>& gt_C, HostVector<float>& C);
+void vector_add(muda::HostVector<float>& gt_C, muda::HostVector<float>& C);
